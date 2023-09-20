07c90b62f643e022907eff2bc06890dded242edd
Ashish Rautela <ashish@avaksh.com>
2023-09-20 08:17:29 +0530

    added navigation component

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -3,7 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useMsal, useMsalAuthentication } from "@azure/msal-react";
 import { InteractionType } from "@azure/msal-browser";
-import SignOut from './components/signOut/SignOut';
+import SignOut from "./components/signOut/SignOut";
+import Navigation from "./components/navigation/Navigation";
 
 function App() {
   useMsalAuthentication(InteractionType.Redirect);
@@ -20,13 +21,16 @@ function App() {
   if (user != "")
     return (
       <div>
-        <div className="welcome">
-          <div>Welcome: {user}</div>
-        </div>
+      <div className="welcome">
+        <div>Welcome: {user}</div>
         <div>
-        <SignOut></SignOut>
+          <SignOut></SignOut>
         </div>
       </div>
+      <div className="nav-wrapper">
+        <Navigation></Navigation>
+      </div>
+    </div>
     );
   else
     return (
--- project/web/src/components/navigation/Navigation.tsx
+++ project/web/src/components/navigation/Navigation.tsx
@@ -1,7 +1,15 @@
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "./Navigation.css";
+
 const Navigation = () => {
-return <div>
-    This is my navigation
-</div>
-}
+  return (
+    <Sidebar>
+      <Menu>
+        <MenuItem> Home </MenuItem>
+        <MenuItem> Admin </MenuItem>
+      </Menu>
+    </Sidebar>
+  );
+};
 
 export default Navigation;
